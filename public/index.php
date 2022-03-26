a1958f9b03321527e1b697089fae11b1bd9488b6
Kate1199 <kate.kisel99@yandex.by>
2022-03-27 00:54:09 +0300

    add root handler

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 define('LARAVEL_START', microtime(true));
 
@@ -53,3 +54,7 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
+ 
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
